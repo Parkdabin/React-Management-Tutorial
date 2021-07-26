35898a4935d5fe11c22ba04309d7aa9c9278ee81
Parkdabin <ekqls4328@naver.com>
2021-07-26 20:58:29 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -22,32 +22,21 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  id: 1,
-  image: 'https://placeimg.com/64/64/1',
-  name: '홍길동',
-  birthday: '970622',
-  gender: '남자',
-  job: '대학생'
-},
-{
-  id: 2,
-  image: 'https://placeimg.com/64/64/2',
-  name: '박다빈',
-  birthday: '970625',
-  gender: '여자',
-  job: '대학생'
-},
-{
-  id: 3,
-  image: 'https://placeimg.com/64/64/3',
-  name: '이구아나',
-  birthday: '940625',
-  gender: '남자',
-  job: '회사원'
-}]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+  componentDidMount(){
+    this.callApi().then(res => this.setState({customers: res})).catch(err => console.log(err));
+  }
+
+  callApi = async()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
 
     const {classes} = this.props;
@@ -65,7 +54,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers ? this.state.customers.map(c => {
               return (
                 <Customer
                   key={c.id}
@@ -77,7 +66,7 @@ class App extends Component {
                   job={c.job}
                 ></Customer>
               )
-            })}
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -5,8 +5,31 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+    res.send([{
+        id: 1,
+        image: 'https://placeimg.com/64/64/1',
+        name: '홍길동',
+        birthday: '970622',
+        gender: '남자',
+        job: '대학생'
+      },
+      {
+        id: 2,
+        image: 'https://placeimg.com/64/64/2',
+        name: '박다빈',
+        birthday: '970625',
+        gender: '여자',
+        job: '대학생'
+      },
+      {
+        id: 3,
+        image: 'https://placeimg.com/64/64/3',
+        name: '이구아나',
+        birthday: '940625',
+        gender: '남자',
+        job: '회사원'
+      }]);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
